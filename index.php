5229fb5769895245831959f7800b088c0550c6b7
Mitchell Shelton <info@mitchellshelton.com>
2015-03-28 19:37:20 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Mitchell Shelton</title>
+    <meta name="description" content="The personal website of Mitchell R.K. Shelton, a Front End Developer living in Washington, D.C.">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+  </head>
+<body>
+
+<a href="#main" class="skip-link" id="skip-to-main">Skip to main content</a>
+
+<header id="header" class="header">
+  <div class="header-content">
+    <h1 class="site-title"><a class="site-title-link" href="#home">Mitchell Shelton</a></h1>
+    <div class="description-section">
+      <p class="description-title">Front End Web Developer</p>
+      <p class="description-location">Washington, D.C.</p>
+    </div>
+  </div>
+</header>
+
+<main id="main" class="main">
+
+  <div class="connect-section">
+    <div class="connect-content">
+      <ul class="connect-list">
+        <li class="connect-item item1"><a class="connect-link linkedin" href="http://www.linkedin.com/in/mitchellshelton"><span class="visuallyhidden">LinkedIn</span></a></li>
+        <li class="connect-item item2"><a class="connect-link github" href="https://github.com/mitchellshelton"><span class="visuallyhidden">Github</span></a></li>
+        <li class="connect-item item3"><a class="connect-link dribbble" href="https://dribbble.com/mitchellshelton"><span class="visuallyhidden">Dribbble</span></a></li>
+        <li class="connect-item item4"><a class="connect-link flickr" href="https://www.flickr.com/photos/mitchell-shelton/"><span class="visuallyhidden">Flickr</span></a></li>
+        <li class="connect-item item5"><a class="connect-link twitter" href="http://twitter.com/MitchellShelton"><span class="visuallyhidden">Twitter</span></a></li>
+        <li class="connect-item item6"><a class="connect-link facebook" href="https://www.facebook.com/Mitchell.R.K.Shelton"><span class="visuallyhidden">Facebook</span></a></li>
+        <li class="connect-item item7"><a class="connect-link googleplus" href="https://plus.google.com/+MitchellShelton/"><span class="visuallyhidden">Google+</span></a></li>
+        <li class="connect-item item8"><a class="connect-link blogger" href="http://mitchellshelton.blogspot.com/"><span class="visuallyhidden">Blogger</span></a></li>
+        <li class="connect-item item9"><a class="connect-link tumblr" href="http://mitchellshelton.tumblr.com/"><span class="visuallyhidden">Tumblr</span></a></li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="project-section">
+    <div class="project-content">
+      <ul class="project-list">
+        <li class="project-item item1"><a class="project-link" href="http://www.juncmodule.com">juncmodule</a></li>
+        <li class="project-item item2"><a class="project-link" href="http://www.keptosh.com">keptosh</a></li>
+      </ul>
+    </div>
+  </div>
+
+</main>
+
+<footer id="footer" class="footer">
+  <div class="footer-content">
+    <p class="footer-copyright">&copy;2015 Mitchell R.K. Shelton</p>
+  </div>
+</footer>
+
+<script src="js/vendor/jquery.min.js"></script>
+<script src="js/vendor/modernizr.min.js"></script>
+<script src="js/script.js"></script>
+
+</body>
+</html>
